f8cba63d5a4a3b7b71c2183f03ac5111bde793ec
J0J0 T <jojo@peek-a-boo.at>
2020-11-06 15:06:07 +0100

    also handle csv files directly

--- members.py
+++ members.py
@@ -4,34 +4,37 @@ import sys
 import csv
 import string
 import zipfile
-#import operator
 import pprint
+import operator
 from subprocess import call
 
-# puts field data to lower case
+
+# returns lower case version of fields data
 def lower_getter(field):
   def _getter(obj):
       return obj[field].lower()
   return _getter
 
 # used like this
-#list_of_dicts.sort(key=lower_getter(key_field)) 
+#list_of_dicts.sort(key=lower_getter(key_field))
 
+print ("")
 if len(sys.argv) > 1:
   filename = sys.argv[1]
   filenamepath=filename.rsplit('/',1)[0]
   filenamefile=filename.rsplit('/',1)[1]
-  print ("file used: " + str(filename))
   if ".zip" in filename:
-    print ("filename contains .zip, extracting "+str(filenamefile)+" to path "+str(filenamepath)+" ...\n")
+    print("filename contains .zip, extracting "+str(filenamefile)+" to "+str(filenamepath)+"\n")
     with zipfile.ZipFile(filename, "r") as z:
       z.extractall(filenamepath)
-      #filename = string.replace(filename, '.zip', '.csv')
       filenamefile = string.replace(filenamefile, '.zip', '.csv')
       filenamefile = "subscribed_"+filenamefile
       filename=filenamepath+"/"+filenamefile
+  else:
+    print("filename does not contain .zip, assuming "+str(filenamefile)+" contains csv data"+"\n")
+    filename=filenamepath+"/"+filenamefile
 else:
-  print "please provide zip or csv file!"
+  print("please provide zip or csv file!")
   raise sys.exit()
 
 # DEBUG dump 1st 200 bytes
@@ -47,34 +50,41 @@ fo = open(filename, 'wb')
 fo.write(data.replace('\x00', ''))
 fo.close()
 
-# guess csv files dialect?
+# guess csv files dialect
 with open(filename, 'rb') as csvfile:
   dialect = csv.Sniffer().sniff(csvfile.read(1024))
   #print dialect.quotechar
   #print dialect.delimiter
   #print dialect.doublequote
-  # what's this?? why needed?
+  # seek back to position 0
   csvfile.seek(0)
 
   reader = csv.DictReader(csvfile, dialect=dialect)
   #print reader.fieldnames 
 
-  # how does data actually look like?
+  # how does data actually look like? comment out whole block for debugging
   #for row in reader:
   #  pp = pprint.PrettyPrinter(indent=4, depth=1)
+  #  # each row contains a dict:
   #  pp.pprint(row)
-  
+  #  # only show the dicts key email:
+  #  pprint.pprint(row["E-Mail Adresse"])
+  #  # show it in lower case
+  #  pprint.pprint(row["E-Mail Adresse"].lower())
 
-  # sort readers data by email field, 2 many options!!!
+  # 3 different ways of sorting
   # -> using itemgetter -> call like this: sorted(.... key=sortkey)
-  # sortkey = operator.itemgetter('E-Mail Adresse') 
-  # -> using special function lower_getter, defined on top
-  #sortedlist = sorted(reader, key=lower_getter('E-Mail Adresse'), reverse=False)
-  # -> using lambda, why te hell was itemgetter necessary??
-  sortedlist = sorted(reader, key=lambda foo: (foo['E-Mail Adresse'].lower()), reverse=False)
+  #    this version is missing the make it lower case part
+  #sortkey = operator.itemgetter('E-Mail Adresse')
+  #sortedlist = sorted(reader, key=sortkey, reverse=False)
+  # -> lambda is used as a one statement function
+  #    instead of creating a named function (def) for the purpose
+  #sortedlist = sorted(reader, key=lambda foo: (foo['E-Mail Adresse'].lower()), reverse=False)
+  # -> or by using the special function lower_getter, defined on top
+  sortedlist = sorted(reader, key=lower_getter('E-Mail Adresse'), reverse=False)
+
   # debug output sortedlist
   #for row in sortedlist:
-    #print string.lower(row['E-Mail Adresse']), row['First Name'], row['Last Name']
   #  print row['E-Mail Adresse'], row['First Name'], row['Last Name']
 
   # write new csv file 
@@ -86,8 +96,9 @@ with open(filename, 'rb') as csvfile:
       #print row['E-Mail Adresse'], row['First Name'], row['Last Name']
       writer.writerow({'E-Mail': row['E-Mail Adresse'], 'First Name': row['First Name'], 'Last Name': row['Last Name']})
 
-print "file written: " + str(filename2)
-print "starting libreoffice calc... "
+print("file written: " + str(filename2))
+print("")
+print("starting libreoffice calc... ")
 call(['/Applications/LibreOffice.app/Contents/MacOS/soffice', '-n', '/Users/jojo/Documents/privat/mr/maillinglist_print_template.ods'])
 #FIXME how to run second LibreOffice instance with NEW document? how does call work?
 # import to NEW document, copy/paste to print_template which should also be already open
